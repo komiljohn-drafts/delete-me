f26c8a7c2f05b437d91ab9b1a7d704ca54dd8931
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-06-27 18:28:43 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,23 +1,15 @@
-"use client";
-
-import { useEffect } from "react";
-
 import MovieDetails from "../_components/MovieDetails/MovieDetails";
-import { IMovieDetails } from "@/api/movies/movies.types";
 
-export default function SingleMoviePage({ params }: { params: { id: string } }) {
-  const getDetails = () =>
-    fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
-      headers: {
-        Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => console.log("repo => ", res));
+export default async function SingleMoviePage({ params }: { params: { id: string } }) {
+  const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
+    headers: {
+      Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
+    },
+  });
 
-  useEffect(() => {
-    getDetails();
-  }, []);
+  const repo = await res.json();
 
-  return <MovieDetails details={[] as unknown as IMovieDetails} />;
+  console.log("repo => ", params);
+
+  return <MovieDetails details={repo.data} />;
 }
