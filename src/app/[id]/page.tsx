b907bf458c3f5289cedbaf80113ddfd8148f198b
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-06-27 18:39:35 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,25 @@
 import MovieDetails from "../_components/MovieDetails/MovieDetails";
 
-export default async function SingleMoviePage({ params }: { params: { id: string } }) {
+async function getData(id: string) {
   const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
     cache: "no-store",
     headers: {
       Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
     },
   });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-  const repo = await res.json();
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function SingleMoviePage({ params }: { params: { id: string } }) {
+  const repo = await getData(params.id as string);
 
   console.log("repo => ", repo);
 
