01adc3b1a000e60545280adf8bb574cf5d49fd84
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-06-27 18:40:39 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,7 @@
 import MovieDetails from "../_components/MovieDetails/MovieDetails";
 
 async function getData(id: string) {
-  const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
+  const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${id}`, {
     cache: "no-store",
     headers: {
       Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
