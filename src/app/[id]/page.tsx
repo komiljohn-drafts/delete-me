339e4290b7a9c82a96d0a55eabd26b64439ee3e0
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-06-27 18:01:19 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,20 +1,15 @@
 import MovieDetails from "../_components/MovieDetails/MovieDetails";
 
-export default async function SingleMoviePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const res = await fetch(
-    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
-    {
-      headers: {
-        Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
-      },
-    }
-  );
+export default async function SingleMoviePage({ params }: { params: { id: string } }) {
+  const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
+    headers: {
+      Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
+    },
+  });
 
   const repo = await res.json();
+
+  console.log("repo => ", repo);
 
   return <MovieDetails details={repo.data} />;
 }
