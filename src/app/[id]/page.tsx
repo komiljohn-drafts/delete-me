127b2d2e3dd036333fde7e924c4e2cfb0461c868
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-06-27 18:32:08 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,6 +2,7 @@ import MovieDetails from "../_components/MovieDetails/MovieDetails";
 
 export default async function SingleMoviePage({ params }: { params: { id: string } }) {
   const res = await fetch(`https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`, {
+    cache: "no-store",
     headers: {
       Authorization: "Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM",
     },
